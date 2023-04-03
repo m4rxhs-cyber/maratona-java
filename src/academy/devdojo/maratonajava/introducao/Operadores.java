1f7e2a6322ca1110c4da6ba80e699ed64bd6af4f
Neemias Sales <neemias.sales@gmail.com>
2023-04-03 00:23:40 -0300

    aula sobre operadores logicos

--- src/academy/devdojo/maratonajava/introducao/Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Operadores.java
@@ -15,5 +15,37 @@ public class Operadores {
         System.out.println(multiplied);
         System.out.println(division);
 
+        // %
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        // Operadores comparação: < > <= >= == !=
+        // Eles sempre irão trazer valores booleando: true ou false
+
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMaiorIgualQueVinte = 10 >= 20;
+        boolean isDezMenorIgualQueVinte = 10 <= 20;
+        boolean isDezIgualQueVinte = 10 == 20;
+        boolean isDezDiferenteQueVinte = 10 != 20;
+
+        System.out.println("isDezMenorQueVinte: "+isDezMenorQueVinte);
+        System.out.println("isDezMaiorQueVinte: "+isDezMaiorQueVinte);
+        System.out.println("isDezMaiorIgualQueVinte: "+isDezMaiorIgualQueVinte);
+        System.out.println("isDezMenorIgualQueVinte: "+isDezMenorIgualQueVinte);
+        System.out.println("isDezIgualQueVinte: "+isDezIgualQueVinte);
+        System.out.println("isDezDiferenteQueVinte: "+isDezDiferenteQueVinte);
+
+        //Operadores Lógicos: && (AND) || (OR)
+
+        //AND
+        int idade = 34;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3338;
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta: "+ isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta: "+ isDentroDaLeiMenorQueTrinta);
+
     }
 }
